7452f74b917d306da707700304a3ca10fd595250
Your Name <you@example.com>
2024-02-18 23:08:23 +0300

    HW3 is completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	size := 8
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			if (i+j)%2 == 0 {
+				fmt.Print(" ")
+			} else {
+				fmt.Print("#")
+			}
+		}
+		fmt.Println()
+	}
 }

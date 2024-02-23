9cc99e12cc2790823468dbd43c5068b82ab1dc29
Your Name <you@example.com>
2024-02-23 14:47:57 +0300

    добавление скана

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
 	var size int
-	fmt.Fscan(os.Stdin, &size)
+	fmt.Print("Введите число: ")
+	fmt.Scanf("%d", &size)
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
 			if (i+j)%2 == 0 {

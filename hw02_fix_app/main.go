d09470d7e0568e71083679964b84005ac12363da
Your Name <you@example.com>
2024-02-28 22:48:25 +0300

    Проверка ошибок

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -14,16 +14,18 @@ func main() {
 	fmt.Printf("Enter data file path: ")
 	_, err := fmt.Scanln(&path)
 
-	var err error
 	var staff []types.Employee
 
-	if len(path) == " " {
+	if len(path) == 0 {
 		path = "data.json"
+	} else {
+
 	}
 
 	staff, err = reader.ReadJSON(path, -1)
-
-	fmt.Print(err)
+	if err != nil {
+		fmt.Print(err)
+	}
 
 	printer.PrintStaff(staff)
 }
--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -11,19 +11,21 @@ import (
 func ReadJSON(filePath string) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		return nil, fmt.Errorf("Error: %v", err)
 	}
 
 	bytes, err := io.ReadAll(f)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return nil, err
+		return nil, fmt.Errorf("Error: %v", err)
+
 	}
 
 	var data []types.Employee
 
 	err = json.Unmarshal(bytes, &data)
-
+	if err != nil {
+		return nil, fmt.Errorf("Error: %v", err)
+	}
 	res := data
 
 	return res, nil
